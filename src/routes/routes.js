e528dcdedcb6e93c690e98803fbcb4272039e282
Mickael Araujo <mickaelaraujo.contato@gmail.com>
2024-02-10 18:59:16 -0300

    create task controller

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,41 +1,9 @@
-import { randomUUID } from 'node:crypto'
-import { getInvalidParameterErrors } from '../utils/get-invalid-parameter-errors.js'
+import taskController from "../controllers/task-controller.js"
 
 export const routes = [
     {
         method: 'POST',
         path: '/tasks',
-        handler: (request, response) => {
-            const errors = getInvalidParameterErrors({
-                title: request.body?.title, 
-                description: request.body?.description,
-            })
-
-            if (errors.length) return response.writeHead(400).end(JSON.stringify({
-                error: true,
-                errors,
-            }))
-
-            const {
-                title,
-                description,
-            } = request.body
-
-            const now = new Date().toISOString()
-
-            const task = {
-                id: randomUUID(),
-                title,
-                description,
-                created_at: now,
-                updated_at: now,
-                completed_at: null,
-            }
-
-            console.log(task)
-
-            return response.writeHead(201)
-                .end()
-        },
+        handler: (request, response) => taskController.create(request, response),
     }
 ]
